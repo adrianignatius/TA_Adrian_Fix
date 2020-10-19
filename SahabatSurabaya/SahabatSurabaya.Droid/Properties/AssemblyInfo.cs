bdf0739b74b52518e433db402f86daa4fa7cd283
Adrian <adrian5@mhs.stts.edu>
2020-10-19 13:52:09 +0700

    Progress

--- SahabatSurabaya/SahabatSurabaya.Droid/Properties/AssemblyInfo.cs
+++ SahabatSurabaya/SahabatSurabaya.Droid/Properties/AssemblyInfo.cs
@@ -17,6 +17,9 @@ using Android.App;
 
 [assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
 [assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
+[assembly: UsesPermission(Android.Manifest.Permission.Internet)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadExternalStorage)]
+[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
 [assembly: UsesFeature("android.hardware.location", Required = false)]
 [assembly: UsesFeature("android.hardware.location.gps", Required = false)]
 [assembly: UsesFeature("android.hardware.location.network", Required = false)]
